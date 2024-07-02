07625ef71dd76403d0a5930e17c5c07e0b838491
Mariopq10 <115182975+Mariopq10@users.noreply.github.com>
2024-07-02 14:43:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BookScout',
+  webDir: 'www'
+};
+
+export default config;
